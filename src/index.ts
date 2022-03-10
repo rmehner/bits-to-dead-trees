d57bbbadf41854b679ea22830dd103738b24dba8
Robin Mehner <robin@coding-robin.de>
2022-03-10 13:18:58 +0100

    Make server port configurable via env

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import fastify from "fastify";
 
+const SERVER_PORT = process.env["SERVER_PORT"] ?? 8000;
+
 const server = fastify();
 
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
 
-server.listen(8080, (err, address) => {
+server.listen(+SERVER_PORT, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
