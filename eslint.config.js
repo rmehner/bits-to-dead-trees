40ea4e7a118c2b160a00b3bff43cd67d1d0c7481
Robin Mehner <robin@coding-robin.de>
2024-10-21 12:04:26 +0200

    Re-do eslint config (those are not tears of joy)

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+// @ts-ignore
+import pluginJs from "@eslint/js";
+// @ts-ignore
+import pluginPromise from "eslint-plugin-promise";
+import nodePlugin from "eslint-plugin-n";
+// @ts-ignore
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node,
+      sourceType: "module",
+      ecmaVersion: 2024,
+    },
+  },
+  pluginJs.configs.recommended,
+  nodePlugin.configs["flat/recommended-module"],
+  {
+    rules: {
+      "n/no-unpublished-import": "off",
+    },
+  },
+  pluginPromise.configs["flat/recommended"],
+  eslintConfigPrettier,
+];
