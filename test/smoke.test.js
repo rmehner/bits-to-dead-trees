8f2a8e4a8aba48b9bf0f8d022fec8131a2506819
Robin Mehner <robin@coding-robin.de>
2025-03-10 11:00:19 +0100

    test: fix deprecation for test options

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -3,38 +3,34 @@ import { describe, expect, it } from "vitest";
 import build from "../app";
 const app = build();
 
-describe(
-  "Smoke tests",
-  () => {
-    it("sends the PDF on the success case", async () => {
-      const response = await app.inject({
-        method: "POST",
-        url: "/pdf",
-        payload: { url: "https://coding-robin.de" },
-      });
-
-      expect(response.payload.startsWith("%PDF")).toBeTruthy();
+describe("Smoke tests", { timeout: 10_000, retry: 3 }, () => {
+  it("sends the PDF on the success case", async () => {
+    const response = await app.inject({
+      method: "POST",
+      url: "/pdf",
+      payload: { url: "https://coding-robin.de" },
     });
 
-    it("returns an error object when the page could not be loaded", async () => {
-      const response = await app.inject({
-        method: "POST",
-        url: "/pdf",
-        payload: {
-          url: "https://httpstat.us/404",
-        },
-      });
+    expect(response.payload.startsWith("%PDF")).toBeTruthy();
+  });
 
-      expect(response.statusCode).toEqual(404);
-      const error = JSON.parse(response.body);
-
-      expect(error).toMatchObject({
-        error: true,
-        status: 404,
-        statusText: "Not Found",
-        message: "404 Not Found",
-      });
+  it("returns an error object when the page could not be loaded", async () => {
+    const response = await app.inject({
+      method: "POST",
+      url: "/pdf",
+      payload: {
+        url: "https://httpstat.us/404",
+      },
     });
-  },
-  { timeout: 10_000, retry: 3 },
-);
+
+    expect(response.statusCode).toEqual(404);
+    const error = JSON.parse(response.body);
+
+    expect(error).toMatchObject({
+      error: true,
+      status: 404,
+      statusText: "Not Found",
+      message: "404 Not Found",
+    });
+  });
+});
